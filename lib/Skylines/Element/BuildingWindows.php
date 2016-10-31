fd6d5d20a9b1aed1599c2394c56099bc640f3316
Ashur Cabrera <hello@cabreramade.co>
2016-10-30 21:52:44 -0700

    added Windows building style

---
+++ lib/Skylines/Element/BuildingWindows.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of Skylines
+ */
+namespace Skylines\Element;
+
+use Huxtable\Core\Utils;
+use Huxtable\Pixel;
+
+class BuildingWindows extends Building
+{
+	/**
+	 * @var	string
+	 */
+	public $type = 'windows';
+
+	/**
+	 *
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+
+		if( $this->width % 2 == 0 )
+		{
+			$this->width++;
+		}
+	}
+
+	/**
+	 * @param	string					$color
+	 * @param	int						$offsetCol
+	 * @param	Huxtable\Pixel\Canvas	$canvas
+	 */
+	public function draw( $color, $offsetCol, Pixel\Canvas &$canvas )
+	{
+		$offsetRow = $canvas->getRows() - $this->getHeight();
+
+		$buildingCanvas = new Pixel\Canvas( $this->width, $this->getHeight(), $canvas->getPixelSize() );
+
+		/* Windows */
+		for( $row = 0; $row < $this->height; $row++ )
+		{
+			if( $row % 2 == 0 )
+			{
+				$buildingCanvas->fillRectangle( 0, $row, $this->width, 1, $color );
+			}
+			else
+			{
+				for( $col = 0; $col < $this->width; $col = $col + 2 )
+				{
+					$buildingCanvas->drawAt( $col, $row, $color );
+				}
+			}
+		}
+
+		$canvas->compositeCanvas( $buildingCanvas, $offsetCol, $offsetRow );
+	}
+
+	/**
+	 * @param	array	$data
+	 * @return	Skylines\Building
+	 */
+	public function getInstanceFromData( array $data )
+	{
+		$building = new self();
+
+		$building->height = $data['height'];
+		$building->leftMargin = $data['leftMargin'];
+		$building->width = $data['width'];
+
+		return $building;
+	}
+}
