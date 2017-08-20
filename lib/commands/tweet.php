d27ff50405dbb9597e48d2c28e352c7f2608d9e7
Ashur Cabrera <hello@cabreramade.co>
2017-08-20 12:45:03 -0700

    added support for scheduled skylines

--- lib/commands/tweet.php
+++ lib/commands/tweet.php
@@ -27,11 +27,39 @@ $commandTweet = new Command( 'tweet', 'Generate a logo and tweet it', function()
 	$jsonFile = $dirTemp->child( 'tweet.json' );
 
 	/*
+	 * Scheduled Events
+	 */
+	$scheduleURL  = "https://cabrera-bots.s3.amazonaws.com/tinyskylines/schedule.json";
+	$scheduleJSON = file_get_contents( $scheduleURL );
+	$schedule = Schedule\Schedule::createFromJSON( $scheduleJSON );
+
+	$currentTime = time();
+	$startTime   = $currentTime - 5 * 60;	// 5-minute window on either side
+	$endTime     = $currentTime + 5 * 60;
+
+	$scheduledEvents = $schedule->getEventsInRange( $startTime, $endTime );
+
+	/* Generate scheduled skyline */
+	if( count( $scheduledEvents ) > 0 )
+	{
+		$event = $scheduledEvents[0];
+
+		$skylineJSON = file_get_contents( $event->getURL() );
+		$skylineData = json_decode( $skylineJSON, true );
+
+		$skyline = Skyline::getInstanceFromData( $skylineData );
+	}
+
+	/* Generate random skyline */
+	else
+	{
+		$palette = $bot->getRandomPalette();
+		$skyline = $bot->getSkyline( $palette );
+	}
+
+	/*
 	 * Render
 	 */
-	$palette = $bot->getRandomPalette();
-	$skyline = $bot->getSkyline( $palette );
-
 	$skyline->render( $imageFile, 150, 50, 5 );
 
 	/*
