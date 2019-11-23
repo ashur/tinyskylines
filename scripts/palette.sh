263740b5c8089750204a37a6e7f0434cedcfaffe
Ashur Cabrera <hello@ashur.cab>
2019-11-23 09:11:04 -0700

    Switch palettes URL to script argument

--- scripts/palette.sh
+++ scripts/palette.sh
@@ -2,6 +2,13 @@
 
 # Select first palette element and save to disk
 
+if [ "$1" == "" ]; then
+	echo "usage: ./palette.sh <url>"
+	exit 1
+fi
+
+PALETTES_URL=$1
+
 SCRIPTS=$(dirname "$0")
 SRC=$SCRIPTS/../
 
@@ -11,7 +18,7 @@ source $SCRIPTS/log.sh
 mkdir -p $TINYSKYLINES_DATADIR
 PALETTE=$TINYSKYLINES_DATADIR/palette.json
 
-PALETTES=`curl -s $TINYSKYLINES_PALETTES_URL`
+PALETTES=`curl -s $PALETTES_URL`
 RESULT=`echo $PALETTES | jq ".[0]" 2>&1 > $PALETTE`
 
 if [ $? -ne 0 ]; then
